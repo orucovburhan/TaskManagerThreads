5dec2670db9ce602a1c952af0cdfb27e0ca6dc2d
Burhan <burhanorucov@gmail.com>
2025-09-26 21:10:14 +0400

    Initial commit

---
+++ TaskManagerThreads/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Net.Mime;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+      
+      while (true)
+      {
+         Console.WriteLine(@"
+1.Show all processes
+2.Kill process
+3.Create a process
+Choose one:");
+
+         string choice = Console.ReadLine();
+         if (choice == "1")
+         {
+            foreach (Process process in Process.GetProcesses())
+            {
+               Console.WriteLine(process.ProcessName);
+            }
+         }
+         else if (choice == "2")
+         {
+            Console.Write("Enter the name of the process that you want to kill:");
+            string processName = Console.ReadLine();
+            var process = Process.GetProcessesByName(processName);
+            process[0].Kill();            
+         }
+         else if (choice == "3")
+         {
+            Console.Write("Enter the name of the process that you want to start:");
+            string processName = Console.ReadLine();
+            var app1 = new ProcessStartInfo
+            {
+               FileName = processName,
+               UseShellExecute = true,
+            };
+            try
+            {
+               Process.Start(processName);
+            }
+            catch
+            {
+               Console.WriteLine("Application couldn't start.");
+            }
+         }
+
+
+
+      }
+   }
+
+
+
+}
+
